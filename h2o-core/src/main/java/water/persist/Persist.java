93194fcdcfbbe4557b61657d5cf15200c8a201a2
Tom Kraljevic <tomk@tomk.net>
2015-03-20 09:29:58 -0700

    First crack at adding HDFS support for NodePersistentStorage. Moved a bunch of stuff down into PersistManager.

--- h2o-core/src/main/java/water/persist/Persist.java
+++ h2o-core/src/main/java/water/persist/Persist.java
@@ -18,29 +18,45 @@ import water.util.Log;
  *  load of files - typically raw text for parsing.
  */
 public abstract class Persist {
-  /** Store a Value into persistent storage, consuming some storage space. */
+  /**
+   * Store a Value into persistent storage, consuming some storage space.
+   */
   abstract public void store(Value v);
 
-  /** Load a previously stored Value */
+  /**
+   * Load a previously stored Value
+   */
   abstract public byte[] load(Value v) throws IOException;
 
-  /** Reclaim space from a previously stored Value */
+  /**
+   * Reclaim space from a previously stored Value
+   */
   abstract public void delete(Value v);
 
-  /** Usable storage space, or -1 for unknown */
-  public long getUsableSpace() { return /*UNKNOWN*/-1; }
+  /**
+   * Usable storage space, or -1 for unknown
+   */
+  public long getUsableSpace() {
+    return /*UNKNOWN*/-1;
+  }
 
-  /** Total storage space, or -1 for unknown */
-  public long getTotalSpace() { return /*UNKNOWN*/-1; }
+  /**
+   * Total storage space, or -1 for unknown
+   */
+  public long getTotalSpace() {
+    return /*UNKNOWN*/-1;
+  }
 
-  /** Transform given uri into file vector holding file name. */
+  /**
+   * Transform given uri into file vector holding file name.
+   */
   abstract public Key uriToKey(URI uri) throws IOException;
 
   /**
    * Calculate typeahead matches for src
    *
    * @param filter Source string to match for typeahead
-   * @param limit Max number of entries to return
+   * @param limit  Max number of entries to return
    * @return List of matches
    */
   abstract public ArrayList<String> calcTypeaheadMatches(String filter, int limit);
@@ -62,7 +78,7 @@ public abstract class Persist {
   }
 
   static String getIceDirectory(Key key) {
-    if( !key.isChunkKey() ) return "not_an_arraylet";
+    if (!key.isChunkKey()) return "not_an_arraylet";
     // Reverse arraylet key generation
     byte[] b = key.getVecKey()._kb;
     return escapeBytes(b, 0, new StringBuilder(b.length)).toString();
@@ -88,18 +104,18 @@ public abstract class Persist {
     // check if we are system key
     StringBuilder sb = new StringBuilder(k._kb.length / 2 + 4);
     int i = 0;
-    if( k._kb[0] < 32 ) {
+    if (k._kb[0] < 32) {
       // System keys: hexalate all the leading non-ascii bytes
       sb.append('%');
       int j = k._kb.length - 1;     // Backwards scan for 1st non-ascii
-      while( j >= 0 && k._kb[j] >= 32 && k._kb[j] < 128 )
+      while (j >= 0 && k._kb[j] >= 32 && k._kb[j] < 128)
         j--;
-      for( ; i <= j; i++ ) {
+      for (; i <= j; i++) {
         byte b = k._kb[i];
         int nib0 = ((b >>> 4) & 15) + '0';
-        if( nib0 > '9' ) nib0 += 'A' - 10 - '0';
+        if (nib0 > '9') nib0 += 'A' - 10 - '0';
         int nib1 = ((b >>> 0) & 15) + '0';
-        if( nib1 > '9' ) nib1 += 'A' - 10 - '0';
+        if (nib1 > '9') nib1 += 'A' - 10 - '0';
         sb.append((char) nib0).append((char) nib1);
       }
       sb.append('%');
@@ -109,19 +125,35 @@ public abstract class Persist {
   }
 
   private static StringBuilder escapeBytes(byte[] bytes, int i, StringBuilder sb) {
-    for( ; i < bytes.length; i++ ) {
-      char b = (char)bytes[i], c=0;
-      switch( b ) {
-      case '%': c='%'; break;
-      case '.': c='d'; break;
-      case '/': c='s'; break;
-      case ':': c='c'; break;
-      case '"': c='q'; break;
-      case '>': c='g'; break;
-      case '\\':c='b'; break;
-      case '\0':c='z'; break;
+    for (; i < bytes.length; i++) {
+      char b = (char) bytes[i], c = 0;
+      switch (b) {
+        case '%':
+          c = '%';
+          break;
+        case '.':
+          c = 'd';
+          break;
+        case '/':
+          c = 's';
+          break;
+        case ':':
+          c = 'c';
+          break;
+        case '"':
+          c = 'q';
+          break;
+        case '>':
+          c = 'g';
+          break;
+        case '\\':
+          c = 'b';
+          break;
+        case '\0':
+          c = 'z';
+          break;
       }
-      if( c!=0 ) sb.append('%').append(c);
+      if (c != 0) sb.append('%').append(c);
       else sb.append(b);
     }
     return sb;
@@ -132,39 +164,97 @@ public abstract class Persist {
     String key = s;
     byte[] kb = new byte[(key.length() - 1) / 2];
     int i = 0, j = 0;
-    if( (key.length() > 2) && (key.charAt(0) == '%') && (key.charAt(1) >= '0') && (key.charAt(1) <= '9') ) {
+    if ((key.length() > 2) && (key.charAt(0) == '%') && (key.charAt(1) >= '0') && (key.charAt(1) <= '9')) {
       // Dehexalate until '%'
-      for( i = 1; i < key.length(); i += 2 ) {
-        if( key.charAt(i) == '%' ) break;
-        char b0 = (char) (key.charAt(i    ) - '0');
-        if( b0 > 9 ) b0 += '0' + 10 - 'A';
+      for (i = 1; i < key.length(); i += 2) {
+        if (key.charAt(i) == '%') break;
+        char b0 = (char) (key.charAt(i) - '0');
+        if (b0 > 9) b0 += '0' + 10 - 'A';
         char b1 = (char) (key.charAt(i + 1) - '0');
-        if( b1 > 9 ) b1 += '0' + 10 - 'A';
+        if (b1 > 9) b1 += '0' + 10 - 'A';
         kb[j++] = (byte) ((b0 << 4) | b1);  // De-hexelated byte
       }
       i++;                      // Skip the trailing '%'
     }
     // a normal key - ASCII with special characters encoded after % sign
-    for( ; i < key.length(); ++i ) {
+    for (; i < key.length(); ++i) {
       byte b = (byte) key.charAt(i);
-      if( b == '%' ) {
-        switch( key.charAt(++i) ) {
-        case '%':  b = '%';  break;
-        case 'c':  b = ':';  break;
-        case 'd':  b = '.';  break;
-        case 'g':  b = '>';  break;
-        case 'q':  b = '"';  break;
-        case 's':  b = '/';  break;
-        case 'b':  b = '\\'; break;
-        case 'z':  b = '\0'; break;
-        default:
-          Log.warn("Invalid format of filename " + s + " at index " + i);
+      if (b == '%') {
+        switch (key.charAt(++i)) {
+          case '%':
+            b = '%';
+            break;
+          case 'c':
+            b = ':';
+            break;
+          case 'd':
+            b = '.';
+            break;
+          case 'g':
+            b = '>';
+            break;
+          case 'q':
+            b = '"';
+            break;
+          case 's':
+            b = '/';
+            break;
+          case 'b':
+            b = '\\';
+            break;
+          case 'z':
+            b = '\0';
+            break;
+          default:
+            Log.warn("Invalid format of filename " + s + " at index " + i);
         }
       }
-      if( j >= kb.length ) kb = Arrays.copyOf(kb, Math.max(2, j * 2));
+      if (j >= kb.length) kb = Arrays.copyOf(kb, Math.max(2, j * 2));
       kb[j++] = b;
     }
     // now in kb we have the key name
     return Key.make(Arrays.copyOf(kb, j));
+  }
+
+  // -------------------------------
+  // Node Persistent Storage helpers
+  // -------------------------------
+
+  public static class PersistEntry {
+    public String _name;
+    public long _size;
+    public long _timestamp_millis;
+  }
+
+  public PersistEntry[] list(String path) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  public boolean exists(String path) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  public long length(String path) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  public InputStream open(String path) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  public boolean mkdirs(String path) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  public boolean rename(String fromPath, String toPath) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  public OutputStream create(String path, boolean overwrite) {
+    throw new RuntimeException("Not implemented");
+  }
+
+  public boolean delete(String path) {
+    throw new RuntimeException("Not implemented");
   }
 }
